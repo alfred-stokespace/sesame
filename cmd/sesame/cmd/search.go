82322e6141339dadac3348a9dc10c2e15819b037
astappenbeck <03m3333@gmail.com>
2022-05-07 07:11:32 -0700

    Adding search command

---
+++ cmd/sesame/cmd/search.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+
+var nickname string
+var tag string
+
+// searchCmd represents the search command
+var searchCmd = &cobra.Command{
+	Use:   "search",
+	Short: "search for a single host from a nickname you provide",
+	Long: `If you have a "Nickname" tag on your host search using that.
+
+If you don't have the default tag name then you can provide it.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("search called: [%s: %s]\n", tag, nickname)
+		
+	},
+}
+
+
+func init() {
+	rootCmd.AddCommand(searchCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// searchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+ 	searchCmd.Flags().StringVarP(&nickname, "nickname", "n", "", "Provide the value (or name) to search SSM hosts by tag value. See additional flag for your custom tag key.")
+	searchCmd.Flags().StringVarP(&tag, "tag", "t", "Nickname", "Provide the value of a tag name to search SSM hosts by tag value.")
+
+	rootCmd.MarkFlagRequired("nickname")
+
+}
