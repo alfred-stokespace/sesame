09b0935195012c8a43e4e64f2ada022ee8dfbfca
astappenbeck <03m3333@gmail.com>
2022-05-07 06:32:25 -0700

    initial code commit

---
+++ cmd/sesame/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/Heraclitus/sesame/cmd/sesame/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Heraclitus/sesame
+
+go 1.16
+
+require github.com/spf13/cobra v1.4.0 // indirect
